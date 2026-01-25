b7dcc1096b8df6dca33e96818028b73d62a86612
KumuIi <chripers08@gmail.com>
2026-01-26 00:14:24 +0100

    vite.confic.jx refrence fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/kumuii-website/', // ← Replace with your GitHub repo name
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+    base: '/', // Change to '/repository-name/' if not using custom domain
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                models: resolve(__dirname, 'models.html'),
+                art: resolve(__dirname, 'art.html'),
+                games: resolve(__dirname, 'games.html'),
+            }
+        }
+    }
 })

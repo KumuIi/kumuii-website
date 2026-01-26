1b1fa8525e703b86bc94944f651e4dce083fdfde
KumuIi <chripers08@gmail.com>
2026-01-26 01:00:20 +0100

    potential fix2

--- src/scene.js
+++ src/scene.js
@@ -1,6 +1,7 @@
-import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+
 
 export function initScene() {
   // Scene setup

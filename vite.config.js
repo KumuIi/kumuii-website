445a5348364e9e50f9c520c24af987ff665b8b08
KumuIi <chripers08@gmail.com>
2026-01-26 02:15:40 +0100

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+    base: '/',
   build: {
     rollupOptions: {
       input: {

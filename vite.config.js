bd6ec6e4090ae73ccca65914d01c45fcaa4861e5
KumuIi <chripers08@gmail.com>
2026-01-26 09:14:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/kumuii-website/',
+    base: '/',
     build: {
         rollupOptions: {
             output: {
